4c5fc64184c078c26ff739d7616d38cc86fb781e
ShilvianaMA <144420393+ShilvianaMA@users.noreply.github.com>
2023-09-22 13:55:09 +0700

    Add files via upload

---
+++ Tugas 2 Praktikum FisKom.py
@@ -0,0 +1,11 @@
+# Menentukan gaya (dalam Newton)
+gaya = 100  # Newton
+
+# Menentukan waktu (dalam detik)
+waktu = 0.1  # detik
+
+# Menghitung impuls (gaya * waktu)
+impuls = gaya * waktu
+
+# Menampilkan hasil
+print("Besar impuls yang diterima oleh bola adalah:", impuls, "Newton-detik atau kg·m/s")
